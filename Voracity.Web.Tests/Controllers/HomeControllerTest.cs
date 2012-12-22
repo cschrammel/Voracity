345e097fa54bd71e2d695289a0c52d903ad1b14d
cschrammel <car.schrammel@gmail.com>
2012-12-21 21:18:38 -0600

    Added MVC4 project with mobile views.  Started UI using jQuery

---
+++ Voracity.Web.Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,34 @@
+﻿using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Voracity.Web.Controllers;
+
+namespace Voracity.Web.Tests.Controllers
+{
+    [TestClass]
+    public class HomeControllerTest
+    {
+        [TestMethod]
+        public void Index()
+        {
+            var controller = new HomeController();
+            var result = controller.Index() as ViewResult;
+            Assert.AreEqual("Voracity", result.ViewBag.Message);
+        }
+
+        [TestMethod]
+        public void About()
+        {
+            var controller = new HomeController();
+            var result = controller.About() as ViewResult;
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void Play()
+        {
+            var controller = new HomeController();
+            var result = controller.Play() as ViewResult;
+            Assert.IsNotNull(result);
+        }
+    }
+}
---
+++ Voracity.Web/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Voracity.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Voracity";
+            return View();
+        }
+
+        public ActionResult Play()
+        {
+            ViewBag.Message = "Play";
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "About";
+            return View();
+        }
+    }
+}
