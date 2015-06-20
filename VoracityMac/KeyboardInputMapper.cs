cae4397d10cbf4fb95799a4ac0d72d91f6cb6a46
Carl Schrammel <carl.schrammel@gmail.com>
2015-06-20 15:54:09 -0500

    Trying out vscode on a mac

---
+++ VoracityMac/KeyboardInputMapper.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+
+namespace VoracityMac
+{
+    public class KeyboardInputMapper
+    {
+        private readonly Hashtable _keys;
+
+        public KeyboardInputMapper()
+        {
+            _keys = new Hashtable
+                        {
+                            {ConsoleKey.W, Directions.South},
+                            {ConsoleKey.X, Directions.North},
+                            {ConsoleKey.D, Directions.East},
+                            {ConsoleKey.A, Directions.West},
+                            {ConsoleKey.Q, Directions.SouthWest},
+                            {ConsoleKey.E, Directions.SouthEast},
+                            {ConsoleKey.Z, Directions.NorthWest},
+                            {ConsoleKey.C, Directions.NorthEast}
+                        };
+        }
+
+        public Directions GetDirection(ConsoleKey key)
+        {
+            if (_keys.ContainsKey(key)) return (Directions) _keys[key];
+            return Directions.None;
+        }
+
+        public ConsoleKey GetKey(Directions direction)
+        {
+            return FindKey(direction);
+        }
+
+        private ConsoleKey FindKey(Directions direction)
+        {
+            foreach (var key in _keys.Keys.Cast<ConsoleKey>().Where(key => (Directions) _keys[key] == direction))
+            {
+                return key;
+            }
+            return ConsoleKey.Enter;
+        }
+    }
+}
---
+++ VoracityMac/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace VoracityMac
+{
+    public class Program
+    {
+        private const int _boardSize = 17;
+        private static Game _gameEngine;
+        private static KeyboardInputMapper _inputMapper;
+
+        private static void Main(string[] args)
+        {
+            var positionFinder = new PositionFinder(_boardSize);
+            var surroundingTileFinder = new SurroundingTileFinder(_boardSize, positionFinder);
+            _gameEngine = new Game(new Board(_boardSize, positionFinder, surroundingTileFinder));
+            _inputMapper = new KeyboardInputMapper();
+            while (_gameEngine.Board.AvailableMoves().Count > 0)
+            {
+                DrawBoard(_gameEngine.Board);
+                ProcessInput();
+            }
+            DrawBoard(_gameEngine.Board);
+            System.Console.WriteLine("\n\nGame Over.  Score: " + _gameEngine.Score() + ".  Press Enter to continue.");
+            System.Console.ReadLine();
+        }
+
+        private static string GetControlsInstructions()
+        {
+            return String.Format(
+                "Keys: Up={0}, Down={1}, Left={2}, Right={3}, \n" +
+                "Up-Left={4}, Up-Right={5}, Down-Left={6}, Down-Right={7}\n\n",
+                _inputMapper.GetKey(Directions.South), _inputMapper.GetKey(Directions.North),
+                _inputMapper.GetKey(Directions.West), _inputMapper.GetKey(Directions.East),
+                _inputMapper.GetKey(Directions.SouthWest), _inputMapper.GetKey(Directions.SouthEast),
+                _inputMapper.GetKey(Directions.NorthWest), _inputMapper.GetKey(Directions.NorthEast));
+        }
+
+        private static void DrawBoard(IBoard board)
+        {
+            System.Console.Clear();
+            System.Console.WriteLine(GetControlsInstructions());
+            int i = 1;
+            foreach (PositionedTile tile in board.Tiles())
+            {
+                if (board.CurrentTile == tile)
+                    DrawColoredSpace(ConsoleColor.Yellow);
+                else
+                {
+                    if (tile.IsActive)
+                        System.Console.Write(tile.Number);
+                    else
+                        DrawColoredSpace(ConsoleColor.Blue);
+                }
+                System.Console.Write("   ");
+                if (i%_boardSize == 0) System.Console.Write("\n\n");
+                i++;
+            }
+        }
+
+        private static void DrawColoredSpace(ConsoleColor background)
+        {
+            System.Console.BackgroundColor = background;
+            System.Console.ForegroundColor = ConsoleColor.White;
+            System.Console.Write(" ");
+            System.Console.ResetColor();
+        }
+
+        private static void ProcessInput()
+        {
+            ConsoleKeyInfo input = System.Console.ReadKey();
+            _gameEngine.Chomp(_inputMapper.GetDirection(input.Key));
+        }
+    }
+}
